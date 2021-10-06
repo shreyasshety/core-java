5142a5f1884db1c34b31b3d4936d6a18efac6710
shreyasshety <shreyes.xworkz@gmail.com>
2021-10-06 12:10:47 +0530

    association and methods

---
+++ SportsShopBranch.java
@@ -0,0 +1,15 @@
+class Brand{
+	String name;
+	String ownerName;
+	final String location="blr";
+	int since;
+	
+Brand(String name,String ownerName)
+{
+	this.name=name;
+	this.ownerName=ownerName;
+}
+Brand()
+{}
+
+}
